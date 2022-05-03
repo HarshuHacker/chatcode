fcfa627bf96aab3b9598aadf670809fba54384cb
Harsh Prasad <harshisindian@gmail.com>
2022-05-03 20:32:27 +0530

    Added Store

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../assets/css/App.css";
+
+function App() {
+  return (
+    <div className="App" style={{fontSize: "11vw", textAlign: "center" ,height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <p style={{margin: "0px"}}>Welcome To <b>ChatCode</b></p>
+    </div>
+  );
+}
+
+export default App;
